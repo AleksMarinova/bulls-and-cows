cdafc9c73da5ee2f6df1ff43c902ceb1b1372c46
Kyle Danny <codingkyle92@gmail.com>
2022-02-14 14:16:40 +0100

    register service and interface

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,14 +1,19 @@
 import { useState } from 'react';
+import { register } from '../services/auth.service'
 
 const Register: React.FC = () => {
 const [username, setUsername] = useState("");
 const [email, setEmail] = useState("");
 const [password, setPassword] = useState("");
 
-const handleSubmit = (e: React.FormEvent)=>{
+const handleSubmit = (e: React.FormEvent) => {
   e.preventDefault();
   e.stopPropagation();
-  console.log(username, email, password);
+  register({username, email, password})
+   .then(res => {
+       console.log(res);
+   })
+  
 }
 
   return (
---
+++ client/src/services/auth.service.ts
@@ -0,0 +1,16 @@
+import { IUser } from './interface'
+const API_URL = 'http://localhost:0666/api/';
+
+export const register = ({email, password, username} : IUser) =>{
+    return fetch(API_URL + 'register', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            email: email,
+            password: password,
+            username: username
+        })
+    })
+}
