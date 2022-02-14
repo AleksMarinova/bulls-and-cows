f43f53f4560778c61c05808719bffd3950b227dc
Kyle Danny <codingkyle92@gmail.com>
2022-02-14 13:48:08 +0100

    reg form

--- client/src/components/Register.tsx
+++ client/src/components/Register.tsx
@@ -1,7 +1,20 @@
-const Register: React.FC = () => {
-  return (
-    <div className="registerContainer">
+import {useState} from 'react';
 
+const Register: React.FC = () => {
+const [username, setUsername] = useState("");
+
+  return (
+    <div className="register-container">
+        <h1>Register</h1>
+        <form>
+           <label>Name</label>
+           <input type="text" placeholder="name" value="name" onChange={(e)=>setUsername(e.target.value)}/>
+           <label>Email</label>
+           <input type="text" placeholder="email"/>
+           <label>Password</label>
+           <input type="text" placeholder="password"/>
+           <button>Register</button>
+        </form>
     </div>
   )
 }
