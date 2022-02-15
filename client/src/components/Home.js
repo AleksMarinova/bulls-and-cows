34daabf156063b37bdefa13e6e2be226a647c48e
Aleksandra Marinova <aleksandra.d.marinova@gmail.com>
2022-02-15 12:40:03 +0100

    added home component and styling for it

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -1,11 +1,15 @@
-import {useNavigate} from 'react-router-dom'
+import {useNavigate} from 'react-router-dom';
+import './Home.css';
+const logo = require('../img/cow.png');
 
 const Home = () => {
     const navigate = useNavigate()
     return (
         <div className="home">
-            <h1>Bulls and Cows</h1>
-            <div className="logo"></div>
+            <h1>Bulls & Cows</h1>
+            <div className="logo">
+                <img src={logo} alt="Bulls and Cows logo"/>
+            </div>
             <button onClick={() => navigate('/login')}>Login</button>
             <button onClick={() => navigate('/register')}>Register</button>
         </div>
