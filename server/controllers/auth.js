e321be05195aee5b39d4d7f99200b8c8f4c42918
Kyle Danny <codingkyle92@gmail.com>
2022-02-15 10:47:51 +0100

    error handling

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -3,5 +3,8 @@ import user from '../schemas/User.js'
 
 export const register = (req, res, next) => {
   const { email, name, password } = req.body;
+  if (user.findOne({ username }) || user.findOne({ email })) {
+    return res.status(400).json({ message: 'Please try again'})
+  }
 }
 
