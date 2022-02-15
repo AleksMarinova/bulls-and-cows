6a2facf179471e8ce6b1857c9f2eb8b530eff46d
Kyle Danny <codingkyle92@gmail.com>
2022-02-15 10:15:29 +0100

    setup db

--- server/index.js
+++ server/index.js
@@ -5,21 +5,33 @@ import bcrypt from 'bcrypt';
 import morgan from 'morgan';
 import dotenv from 'dotenv';
 import authRoutes from './routes/auth.js';
-// import { register } from '../controllers/auth.js'
+import mongoose from 'mongoose';
 
 const app = express();
 const PORT = process.env.PORT || 8001;
 
 dotenv.config();
+
+const DB_URL = process.env.DATABASE_URL;
+
+mongoose.connect(DB_URL).then(response => {
+  app.listen(PORT, () => console.log(`Server is running on ${PORT} and connected to DB`));
+}).catch(err => console.error(err));
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(morgan('dev'));
 
 app.use('/api', authRoutes);
 
-app.listen(PORT, () => console.log(`Server is running on ${PORT}`));
-
 // app.use((err, req, res, next) => {
 //   console.log('im the error handler')
 //   res.status(500).json({ message: err.message });
 // });
+
+process.on('SIGINT', () => {
+  mongoose.connection.close(() => {
+    console.log('disconnected from DB');
+    process.exit(0);
+  });
+});
