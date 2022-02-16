7f46585ebe1eaf777ee366d4666350a85063777d
Kyle Danny <codingkyle92@gmail.com>
2022-02-16 09:50:30 +0100

    login logic

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -1,31 +1,46 @@
-import './Login.css';
-import { useNavigate } from 'react-router-dom';
-const image = require('../img/wellies.png');
+import { useState, useEffect } from 'react';
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
+const image = require("../img/wellies.png");
 
+const Login = () => {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
 
+  const handleSubmit = (e: React.FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    const user = {
+      email,
+      password,
+    }
+    // post req
+  }
 
-const Login = () => { 
-
-    const navigate = useNavigate();
-
-    return (
-        <div className="login-container">
-            <div>
-                <img src={image} alt="crops" className="login-image"/>
-            </div>
-            <div className="login-form">
-                <form>
-                    <input type="text" className="login-input" placeholder="Username"/>
-                    <input type="password" className="login-input" placeholder="Password"/>
-                    <button>Login</button>
-                </form>    
-             </div>   
-             <div className="login-redirect-container">
-                <p>Don't have an account?</p>
-                <button onClick = {() => navigate('/register')} >Register</button>
-             </div>   
-        </div>
-    )
-}
+  return (
+    <div className="login-container">
+      <div>
+        <img src={image} alt="crops" className="login-image" />
+      </div>
+      <div className="login-form">
+        <form onSubmit={(e)=> handleSubmit(e)}>
+          <input type="text" className="login-input" placeholder="Email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+          <input
+            type="password"
+            className="login-input"
+            placeholder="Password"
+            value={password} onChange={(e)=>setPassword(e.target.value)}
+          />
+          <button>Login</button>
+        </form>
+      </div>
+      <div className="login-redirect-container">
+        <p>Don't have an account?</p>
+        <button onClick={() => navigate("/register")}>Register</button>
+      </div>
+    </div>
+  );
+};
 
 export default Login;
--- client/src/services/auth.service.ts
+++ client/src/services/auth.service.ts
@@ -1,5 +1,6 @@
 import { IUser } from './interface'
-const API_URL = 'https://bulls-and-cows-server.herokuapp.com/api/';
+// const API_URL = 'https://bulls-and-cows-server.herokuapp.com/api/';
+const API_URL = 'http://localhost:8001/api/';
 
 export const register = async ({ email, password, username }: IUser) =>{
     return await fetch(API_URL + 'register', {
