89751d185c3710ca91e5563f8569d9127b6a2a3a
Kyle Danny <codingkyle92@gmail.com>
2022-02-17 11:06:52 +0100

    submit form with players number to socketio

--- client/src/components/Gameplay.tsx
+++ client/src/components/Gameplay.tsx
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import React, { useState } from 'react'
 
 interface IGameplayProps {
   user: Object;
@@ -9,11 +9,19 @@ interface IGameplayProps {
 const Gameplay = ({user, room, socket}:IGameplayProps) => {
   const [playerNumberChosen, setPlayerNumberChosen] = useState(false);
   const [myNumber, setMyNumber] = useState('');
+  const [opponentsNumber, setOpponentsNumber] = useState('');
 
-  const playerGameNumber = () => {
+  const submitMyNumber = async (e: React.FormEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    await socket.emit('submit_my_number', {room, user, number: myNumber});
+    setPlayerNumberChosen(true);
+  }
+
+  const PlayerGameNumber = () => {
     return (
       <div>
-        <form action="" onSubmit={(e) => setPlayerNumberChosen(true)}>
+        <form onSubmit={(e) => submitMyNumber(e)}>
           <input type="text" onChange={(e) => setMyNumber(e.target.value)}/>
           <button type="submit">Confirm</button>
         </form>
@@ -21,10 +29,22 @@ const Gameplay = ({user, room, socket}:IGameplayProps) => {
     )
   }
 
+  const Board = () => {
+    return (
+      <div>
+        <div className="guesses"></div>
+        <div> <h3>opponent's number: {opponentsNumber}</h3> </div>
+        <input type="text" />
+        <button>guess</button>
+        <div> <h3>my number: {myNumber}</h3> </div>
+      </div>
+    )
+  }
+
   return (
     <div>
       <h1>Bulls and Cows</h1>
-      {playerNumberChosen ? <div>hey</div> : playerGameNumber()}
+      {playerNumberChosen ? Board() : PlayerGameNumber()}
     </div>
     
   )
