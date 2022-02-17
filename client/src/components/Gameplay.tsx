60e6b06991bad78c3b00913695faaa2f555d5f56
Kyle Danny <codingkyle92@gmail.com>
2022-02-17 09:58:31 +0100

    gameplay form

--- client/src/components/Gameplay.tsx
+++ client/src/components/Gameplay.tsx
@@ -1,3 +1,5 @@
+import { useState } from 'react'
+
 interface IGameplayProps {
   user: Object;
   room: string;
@@ -5,9 +7,26 @@ interface IGameplayProps {
 }
 
 const Gameplay = ({user, room, socket}:IGameplayProps) => {
+  const [playerNumberChosen, setPlayerNumberChosen] = useState(false);
+  const [myNumber, setMyNumber] = useState('');
+
+  const playerGameNumber = () => {
+    return (
+      <div>
+        <form action="" onSubmit={(e) => setPlayerNumberChosen(true)}>
+          <input type="text" onChange={(e) => setMyNumber(e.target.value)}/>
+          <button type="submit">Confirm</button>
+        </form>
+      </div>
+    )
+  }
 
   return (
-    <h1>Gameplay</h1>
+    <div>
+      <h1>Bulls and Cows</h1>
+      {playerNumberChosen ? <div>hey</div> : playerGameNumber()}
+    </div>
+    
   )
 }
 
