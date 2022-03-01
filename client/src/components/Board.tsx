b739653d57f415b474643f57c00d03b993b6c531
Kyle Danny <codingkyle92@gmail.com>
2022-03-01 19:24:05 +0100

    added css to board page

--- client/src/components/Board.tsx
+++ client/src/components/Board.tsx
@@ -4,6 +4,10 @@ import { useState } from 'react';
 import { validatePlayerNumber, calculateBullsAndCows } from '../utils/utils';
 import { useNavigate } from "react-router-dom";
 
+const cow = require("../img/angry-cow.png");
+const bull = require("../img/angry-bull.png");
+
+
 const Board = ({opponentsNumber, myNumber, socket, myInitialTurn, room, user}: IBoardProps) => {
   const [myTurn, setMyTurn] = useState<boolean>(myInitialTurn);
   const [currentGuess, setCurrentGuess] = useState<string>('');
@@ -48,12 +52,19 @@ const Board = ({opponentsNumber, myNumber, socket, myInitialTurn, room, user}: I
 
   return (
     <div className="gameplay-container" >
-      <div className="gameplay-title" > <h1>Cows 'n Bulls</h1> </div>
+      <div className="gameplay-title" > <h1>Bulls 'n Cows </h1> </div>
       <div className="gameplay-myNumber" > <h5>my number: {myNumber}</h5> </div>
+      { myTurn ? 
+        <div><img src={cow} alt="crops" className="board-image" /></div>
+        : <div><img src={bull} alt="crops" className="board-image" /></div>
+      }
       <div className="guesses"> {guesses.map((guess: IGuess, index: number ) => {
-        return <div className="guess" key={index} >{guess.guess} -- Bulls: {guess.bulls} -- Cows: {guess.cows}</div>;
+        return <div className="guess" key={index} >
+          <div>{guess.guess}</div> 
+          <div>Bulls: {guess.bulls} </div> 
+          <div>Cows: {guess.cows}</div> 
+        </div>;
       })}
-      
        </div>
       
       {/* {!youLost && !youWon ? null : youLost ? <p>You Lost! Opponent's number was {opponentsNumber}</p> : <p>You Won!</p>} */}
@@ -65,10 +76,13 @@ const Board = ({opponentsNumber, myNumber, socket, myInitialTurn, room, user}: I
             <button type="submit">guess</button>
            </form>
          </div> 
-         :  <div className="guess-form">waiting for opponent's turn</div>
+         : <div className="guess-form" >
+            <form onSubmit={(e)=>submitGuess(e)} >
+              <input type="text" value={currentGuess} onChange={(e) => setCurrentGuess(e.target.value)} />
+              <button type="submit" disabled={true}> waiting ... </button>
+            </form>
+          </div> 
       }
-      
-      
     </div>
   )
 }
